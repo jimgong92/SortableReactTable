4441f9b5d103f6bdd34f2995ebcb644964427e8d
Jimmy Gong <gong.jim@gmail.com>
2015-05-13 23:23:59 -0700

    Adds express server to aid testing

---
+++ index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/dist'));
+app.set('port', process.env.PORT || 8000);
+app.listen(app.get('port'), function(){
+  console.log("Listening on port %d", app.get('port'));
+});
